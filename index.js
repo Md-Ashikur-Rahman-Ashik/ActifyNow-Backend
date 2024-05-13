e81bff5f3e76f54b3dcade74a879cddc4a4b838e
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-05-13 12:44:24 +0600

    Deployed on vercel

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://actifynow-bd532.web.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   })
